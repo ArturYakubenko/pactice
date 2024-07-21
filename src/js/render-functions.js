da262cf8cbb5e7b5922b9789afebb88362d71644
artur <Arturyakubenko1993@gmail.com>
2024-07-22 00:15:37 +0300

    fdgf

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -4,11 +4,19 @@ import 'simplelightbox/dist/simple-lightbox.min.css'
 let simplelightbox = null
 
 export function createLi(arr) {
-    return arr.map(({webformatURL, largeImageURL }) => {
-        return `<li>
+    return arr.map(({webformatURL, largeImageURL, tags, views, comments, downloads, likes }) => {
+        return `<li class="item">
+        <div class="item_img">
         <a href="${largeImageURL}">
-        <img src="${webformatURL}" alt="">
+        <img src="${webformatURL}" alt="${tags}">
         </a>
+        </div>
+        <ul class="description">
+            <li><p>views</p><p>${views}</p></li>
+            <li><p>comments</p><p>${comments}</p></li>
+            <li><p>downloads</p><p>${downloads}</p></li>
+            <li><p>likes</p><p>${likes}</p></li>
+        </ul>
         </li>`
     }).join('')
    
